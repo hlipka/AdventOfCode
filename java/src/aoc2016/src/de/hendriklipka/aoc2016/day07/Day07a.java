d4e4ef0ee323a62b553f7ff59d92a50896d8b87f
hli <hli-papermoon@hendriklipka.de>
2023-11-05 22:39:03 +0100

    add 2016 day 7 first part)

---
+++ java/src/aoc2016/src/de/hendriklipka/aoc2016/day07/Day07a.java
@@ -0,0 +1,83 @@
+package de.hendriklipka.aoc2016.day07;
+
+import de.hendriklipka.aoc.AocParseUtils;
+import org.apache.commons.lang3.StringUtils;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * User: hli
+ * Date: 05.11.23
+ * Time: 22:14
+ */
+public class Day07a
+{
+    public static void main(String[] args)
+    {
+        try
+        {
+            int tls = 0;
+            final List<String> addresses = AocParseUtils.getLines("2016", "day07");
+            for (String addr : addresses)
+            {
+                if (canDoTLS(addr))
+                {
+                    System.out.println("TLS for "+addr);
+                    tls++;
+                }
+            }
+            System.out.println(tls);
+        }
+        catch (IOException e)
+        {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static boolean canDoTLS(final String addr)
+    {
+        final String[] split = StringUtils.splitByCharacterType(addr);
+        List<String> outside = new ArrayList<>();
+        List<String> inside = new ArrayList<>();
+        boolean isOutside=true;
+        for (String part: split)
+        {
+            if ("[".equals(part))
+            {
+                isOutside=false;
+            }
+            else if ("]".equals(part))
+            {
+                isOutside = true;
+            }
+            else if (isOutside)
+            {
+                outside.add(part);
+            }
+            else
+            {
+                inside.add(part);
+            }
+        }
+        if (!hasAbba(outside)) return false;
+        return inside.stream().noneMatch(Day07a::hasAbba);
+    }
+
+    private static boolean hasAbba(final List<String> parts)
+    {
+        return parts.stream().anyMatch(Day07a::hasAbba);
+    }
+
+    private static boolean hasAbba(final String part)
+    {
+        int len=part.length();
+        for (int i=0;i<len-3;i++)
+        {
+            if (part.charAt(i)!=part.charAt(i+1)&&part.charAt(i)==part.charAt(i+3)&&part.charAt(i+1)==part.charAt(i+2))
+                return true;
+        }
+        return false;
+    }
+}
