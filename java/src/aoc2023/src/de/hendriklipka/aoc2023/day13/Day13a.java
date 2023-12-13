f0fe3d99af56c0ef24a4a7c72c93ec9ad7fdafbc
hlipka <hli@hendriklipka.de>
2023-12-13 12:03:28 +0100

    add 2023 day 13

---
+++ java/src/aoc2023/src/de/hendriklipka/aoc2023/day13/Day13a.java
@@ -0,0 +1,88 @@
+package de.hendriklipka.aoc2023.day13;
+
+import de.hendriklipka.aoc.AocParseUtils;
+import org.apache.commons.lang3.StringUtils;
+
+import java.io.IOException;
+import java.util.List;
+
+public class Day13a
+{
+    public static void main(String[] args)
+    {
+        try
+        {
+            int sum= AocParseUtils.getStringBlocks("2023", "day13").stream().mapToInt(Day13a::getReflectionValue).sum();
+            System.out.println(sum);
+            // 18259 is too low
+
+        }
+        catch (IOException e)
+        {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static int getReflectionValue(List<String> block)
+    {
+        int i = getRowReflection(block) + getColumnReflection(block);
+        if (0==i)
+            System.err.println("no reflection for\n" + StringUtils.join(block,"\n"));
+        return i;
+    }
+
+    private static int getRowReflection(List<String> block)
+    {
+        // this is the row _before_ the reflection
+        for (int row=1;row<block.size();row++)
+        {
+            if (isRowReflected(block, row))
+                return 100*row;
+        }
+        return 0;
+    }
+
+
+    private static boolean isRowReflected(List<String> block, int row)
+    {
+        int rowCount=Math.min(row, block.size()-row);
+        for (int diff=0;diff<rowCount;diff++)
+        {
+            if (!block.get(row+diff).equals(block.get(row-1-diff)))
+                return false;
+        }
+        return true;
+    }
+
+    private static int getColumnReflection(List<String> block)
+    {
+        int cols=block.get(0).length();
+        for (int col=1;col<cols;col++)
+        {
+            if (isColumnReflected(col, block))
+                return col;
+        }
+        return 0;
+    }
+
+    private static boolean isColumnReflected(int col, List<String> block)
+    {
+        for (String line: block)
+        {
+            if (!isLineReflected(line, col))
+                return false;
+        }
+        return true;
+    }
+
+    private static boolean isLineReflected(String line, int col)
+    {
+        int colCount=Math.min(col, line.length()-col);
+        for (int diff = 0; diff < colCount; diff++)
+        {
+            if (line.charAt(col+diff)!=line.charAt(col-1-diff))
+                return false;
+        }
+        return true;
+    }
+}
---
+++ java/src/aoc2023/src/de/hendriklipka/aoc2023/day13/Day13b.java
@@ -0,0 +1,121 @@
+package de.hendriklipka.aoc2023.day13;
+
+import de.hendriklipka.aoc.AocParseUtils;
+import org.apache.commons.lang3.StringUtils;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Day13b
+{
+    public static void main(String[] args)
+    {
+        try
+        {
+            int sum= AocParseUtils.getStringBlocks("2023", "day13").stream().mapToInt(Day13b::getReflectionValue).sum();
+            System.out.println(sum);
+        }
+        catch (IOException e)
+        {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static int getReflectionValue(List<String> block)
+    {
+        int oldRowValue=getRowReflection(block, -1);
+        int oldColValue=getColumnReflection(block, -1);
+        for (int row=0;row<block.size();row++)
+        {
+            for (int col=0;col<block.get(0).length();col++)
+            {
+                List<String> newBlock=fixBlock(block, row, col);
+                int rowReflection = getRowReflection(newBlock, oldRowValue/100);
+                int columnReflection = getColumnReflection(newBlock, oldColValue);
+                if (0!=rowReflection && rowReflection!=oldRowValue)
+                {
+                    return rowReflection;
+                }
+                if (0!=columnReflection && columnReflection!=oldColValue)
+                {
+                    return columnReflection;
+                }
+            }
+        }
+        System.err.println("no reflection found for\n" + StringUtils.join(block,"\n")+"\nold value was "+(oldRowValue+oldColValue));
+        return 0;
+    }
+
+    private static List<String> fixBlock(List<String> block, int rowFix, int colFix)
+    {
+        List<String> newBlock = new ArrayList<>();
+        for (int row=0;row<block.size();row++)
+        {
+            String s=block.get(row);
+            if (row==rowFix)
+            {
+                char c=s.charAt(colFix);
+                s=s.substring(0, colFix)+('.'==c?'#':'.')+s.substring(colFix+1);
+            }
+            newBlock.add(s);
+        }
+        return newBlock;
+    }
+
+    private static int getRowReflection(List<String> block, int skipRow)
+    {
+        // this is the row _before_ the reflection
+        for (int row=1;row<block.size();row++)
+        {
+            if (row!=skipRow && isRowReflected(block, row))
+                return 100*row;
+        }
+        return 0;
+    }
+
+
+    private static boolean isRowReflected(List<String> block, int row)
+    {
+        int rowCount=Math.min(row, block.size()-row);
+        for (int diff=0;diff<rowCount;diff++)
+        {
+            if (!block.get(row+diff).equals(block.get(row-1-diff)))
+                return false;
+        }
+        return true;
+    }
+
+    private static int getColumnReflection(List<String> block, int skipCol)
+    {
+        int cols=block.get(0).length();
+        // this is the column _before_ the reflection
+        for (int col=1;col<cols;col++)
+        {
+            if (col!=skipCol && isColumnReflected(col, block))
+                return col;
+        }
+        return 0;
+    }
+
+    private static boolean isColumnReflected(int col, List<String> block)
+    {
+        for (String line: block)
+        {
+            if (!isLineReflected(line, col))
+                return false;
+        }
+        return true;
+    }
+
+    private static boolean isLineReflected(String line, int col)
+    {
+        int colCount=Math.min(col, line.length()-col);
+        for (int diff = 0; diff < colCount; diff++)
+        {
+            if (line.charAt(col+diff)!=line.charAt(col-1-diff))
+                return false;
+        }
+        return true;
+    }
+}
