0f32fb990b950b9f3deb11ac5e382c834d50a0a1
hli <hli-papermoon@hendriklipka.de>
2024-09-08 21:42:29 +0200

    add 2016 day 16

---
+++ java/src/aoc2016/src/de/hendriklipka/aoc2016/day16/Day16a.java
@@ -0,0 +1,86 @@
+package de.hendriklipka.aoc2016.day16;
+
+/**
+ * User: hli
+ * Date: 9/8/24
+ * Time: 8:35 PM
+ */
+public class Day16a
+{
+    public static void main(String[] args)
+    {
+        DiskFiller df = new DiskFiller("01111001100111011", 272);
+//        DiskFiller df = new DiskFiller("10000", 20);
+        df.fillDisk();
+        CheckSum cs = new CheckSum(df.getData());
+        System.out.println(cs.getCheckSum());
+    }
+
+    private static class DiskFiller
+    {
+        private String _data;
+        private final int _len;
+        private final String _startState;
+
+        public DiskFiller(String state, int len)
+        {
+            _startState = state;
+            _len = len;
+        }
+
+        private void fillDisk()
+        {
+            StringBuilder current = new StringBuilder(_startState);
+            while (current.length() < _len)
+            {
+                int l = current.length();
+                current.append("0");
+                for (int i=0;i<l;i++)
+                {
+                    char c = current.charAt(l-i-1);
+                    current.append(c=='0'?'1':'0');
+                }
+                System.out.println("d="+current);
+            }
+            _data = current.substring(0,_len);
+        }
+
+        public String getData()
+        {
+            System.out.println(_data);
+            return _data;
+        }
+    }
+
+    private static class CheckSum
+    {
+        private String _checkSum;
+
+        public CheckSum(String data)
+        {
+            _checkSum = data;
+        }
+
+        public String getCheckSum()
+        {
+            while ( 0== (_checkSum.length()%2))
+            {
+                StringBuilder data =new StringBuilder();
+                for (int i=0;i<_checkSum.length()/2;i++)
+                {
+                    if (_checkSum.charAt(2*i)==_checkSum.charAt(2*i+1))
+                    {
+                        data.append('1');
+                    }
+                    else
+                    {
+                        data.append('0');
+                    }
+                }
+                _checkSum = data.toString();
+                System.out.println("cs="+data);
+            }
+            return _checkSum;
+        }
+    }
+}
