90214661cad0938d61b33d3681cdb4da906fe01d
hlipka <hli@hendriklipka.de>
2024-12-05 08:55:16 +0100

    add 2024 day 5

--- java/src/aoc2024/src/de/hendriklipka/aoc2024/day05/Day05.java
+++ java/src/aoc2024/src/de/hendriklipka/aoc2024/day05/Day05.java
@@ -1,8 +1,11 @@
 package de.hendriklipka.aoc2024.day05;
 
+import de.hendriklipka.aoc.AocParseUtils;
 import de.hendriklipka.aoc.AocPuzzle;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Day05 extends AocPuzzle
 {
@@ -19,12 +22,83 @@ public class Day05 extends AocPuzzle
     @Override
     protected Object solvePartA() throws IOException
     {
-        return null;
+        final List<List<String>> blocks = AocParseUtils.getStringBlocks(getYear(), getDay());
+        List<Rule> rules = blocks.get(0).stream().map(Rule::new).toList();
+        List<List<Integer>> updates = blocks.get(1).stream().map(this::parseUpdate).toList();
+
+        return updates.stream().filter(u-> rules.stream().allMatch(r->r.isUpdateOK(u))).mapToInt(u-> u.get(u.size() / 2)).sum();
+    }
+
+    private List<Integer> parseUpdate(final String line)
+    {
+        String[] nums = line.split(",");
+        final List<Integer> update=new ArrayList<>(nums.length);
+        for (String s: nums)
+            update.add(Integer.parseInt(s));
+        return update;
     }
 
     @Override
     protected Object solvePartB() throws IOException
     {
-        return null;
+        final List<List<String>> blocks = AocParseUtils.getStringBlocks(getYear(), getDay());
+        List<Rule> rules = blocks.get(0).stream().map(Rule::new).toList();
+        List<List<Integer>> updates = blocks.get(1).stream().map(this::parseUpdate).toList();
+
+        return updates.stream().filter(u-> !rules.stream().allMatch(r -> r.isUpdateOK(u))).map(u->fixUpdate(u, rules)).mapToInt(u1 -> u1.get(u1.size() / 2)).sum();
+    }
+
+    private List<Integer> fixUpdate(final List<Integer> u, final List<Rule> rules)
+    {
+        final List<Integer> result = new ArrayList<>(u);
+        // checkAndFixUpdate() swaps the two wrong numbers with each other 8when they are not correct already)
+        // so this is basically a bubble sort, but we compare remote pairs instead of direct neighbours
+        // as long as there are no conflicting rules this will converge eventually
+        while (true)
+        {
+            boolean allSorted=true;
+            for (Rule r: rules)
+            {
+                allSorted &= r.checkAndFixUpdate(result);
+            }
+            if (allSorted)
+                break;
+        }
+        return result;
+    }
+
+    private static class Rule
+    {
+
+        private final int _first;
+        private final int _second;
+
+        public Rule(final String l)
+        {
+            _first=AocParseUtils.parseIntFromString(l, "(\\d+)\\|.*");
+            _second=AocParseUtils.parseIntFromString(l, ".*\\|(\\d+)");
+        }
+
+        public boolean isUpdateOK(final List<Integer> update)
+        {
+            int firstPos=update.indexOf(_first);
+            int secondPos=update.indexOf(_second);
+            if (firstPos==-1||secondPos==-1)
+                return true;
+            return firstPos<secondPos;
+        }
+
+        public boolean checkAndFixUpdate(final List<Integer> update)
+        {
+            int firstPos=update.indexOf(_first);
+            int secondPos=update.indexOf(_second);
+            if (firstPos==-1||secondPos==-1)
+                return true;
+            if (firstPos<secondPos)
+                return true;
+            update.set(secondPos, _first);
+            update.set(firstPos, _second);
+            return false;
+        }
     }
 }
---
+++ java/src/aoc2024/src/de/hendriklipka/aoc2024/day06/Day06.java
@@ -0,0 +1,30 @@
+package de.hendriklipka.aoc2024.day06;
+
+import de.hendriklipka.aoc.AocPuzzle;
+
+import java.io.IOException;
+
+public class Day06 extends AocPuzzle
+{
+    public Day06()
+    {
+        super("2024", "05");
+    }
+
+    public static void main(String[] args)
+    {
+        new Day06().doPuzzle(args);
+    }
+
+    @Override
+    protected Object solvePartA() throws IOException
+    {
+        return null;
+    }
+
+    @Override
+    protected Object solvePartB() throws IOException
+    {
+        return null;
+    }
+}
