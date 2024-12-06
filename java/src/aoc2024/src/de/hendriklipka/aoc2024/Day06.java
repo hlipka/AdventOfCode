45e71556aead9e70a25ae7721ec9770f6e0754c4
hli <hli-papermoon@hendriklipka.de>
2024-12-06 18:26:00 +0100

    add 2024 day 6

--- java/src/aoc2024/src/de/hendriklipka/aoc2024/Day06.java
+++ java/src/aoc2024/src/de/hendriklipka/aoc2024/Day06.java
@@ -1,8 +1,17 @@
 package de.hendriklipka.aoc2024;
 
 import de.hendriklipka.aoc.AocPuzzle;
+import de.hendriklipka.aoc.Direction;
+import de.hendriklipka.aoc.Position;
+import de.hendriklipka.aoc.matrix.CharMatrix;
+import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.apache.commons.lang3.tuple.Pair;
 
 import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+
+import static de.hendriklipka.aoc.Direction.*;
 
 public class Day06 extends AocPuzzle
 {
@@ -14,13 +23,83 @@ public class Day06 extends AocPuzzle
     @Override
     protected Object solvePartA() throws IOException
     {
-        data.getLines();
-        return null;
+        CharMatrix lab = data.getLinesAsCharMatrix(' ');
+        Position current = lab.findFirst('^');
+        Set<Position> visited = findVisitedPlaces(lab, current);
+        return visited.size();
     }
 
     @Override
     protected Object solvePartB() throws IOException
     {
-        return null;
+        CharMatrix lab = data.getLinesAsCharMatrix(' ');
+        Position start = lab.findFirst('^');
+
+        // we need to place obstacles only onto visited places, anything else will never be seen by the guard
+        Set<Position> visited = findVisitedPlaces(lab, start);
+        // but we need to remove the start position
+        visited.remove(start);
+
+        return (int) visited.stream().parallel().filter(p -> isLoop(copyLab(lab, p), start)).count();
+    }
+
+    private static Set<Position> findVisitedPlaces(final CharMatrix lab, Position current)
+    {
+        // we just track where we have been
+        Set<Position> visited = new HashSet<>();
+        // both fit sample+real data for me
+        Direction dir = UP;
+        // and walk until we leave the room
+        while (lab.in(current))
+        {
+            visited.add(current);
+            while (true)
+            {
+                Position newPos = current.updated(dir);
+                if (lab.at(newPos) != '#')
+                {
+                    current = newPos;
+                    break;
+                }
+                dir = dir.right();
+            }
+        }
+        return visited;
+    }
+
+    private boolean isLoop(final CharMatrix lab, final Position start)
+    {
+        // we are in a loop when we re-visited a place while going into the same direction
+        Set<Pair<Position, Direction>> visited = new HashSet<>();
+        Position current = start;
+        Direction dir = Direction.UP;
+        while (lab.in(current))
+        {
+            Pair<Position, Direction> p = new ImmutablePair<>(current, dir);
+            if (visited.contains(p))
+            {
+                return true;
+            }
+            visited.add(p);
+            while (true)
+            {
+                Position newPos = current.updated(dir);
+                if (lab.at(newPos) != '#')
+                {
+                    current = newPos;
+                    break;
+                }
+                dir = dir.right();
+            }
+        }
+
+        return false;
+    }
+
+    private CharMatrix copyLab(final CharMatrix lab, final Position p)
+    {
+        CharMatrix copy = lab.copyOf();
+        copy.set(p, '#');
+        return copy;
     }
 }
