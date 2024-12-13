b76b76ea18de3699f881e48c6795810065b4817a
Hendrik Lipka <hli@hendriklipka.de>
2024-12-13 11:58:33 +0100

    add utility to solve an integer linear equation system (two equations)

--- java/src/aoc2024/src/de/hendriklipka/aoc2024/Day13.java
+++ java/src/aoc2024/src/de/hendriklipka/aoc2024/Day13.java
@@ -2,7 +2,7 @@ package de.hendriklipka.aoc2024;
 
 import de.hendriklipka.aoc.AocParseUtils;
 import de.hendriklipka.aoc.AocPuzzle;
-import org.apache.commons.math3.linear.*;
+import de.hendriklipka.aoc.MathUtils;
 
 import java.io.IOException;
 import java.util.List;
@@ -36,7 +36,7 @@ public class Day13 extends AocPuzzle
     @Override
     protected Object solvePartB() throws IOException
     {
-        return data.getStringBlocks().stream().map(this::createRealMachine).parallel().mapToLong(Machine::solveLarge).sum();
+        return data.getStringBlocks().stream().map(this::createRealMachine).mapToLong(Machine::solve).sum();
     }
 
     private static class Machine
@@ -55,62 +55,10 @@ public class Day13 extends AocPuzzle
 
         public long solve()
         {
-            long minCoins=Long.MAX_VALUE;
-            for (int aSteps=0; aSteps<101; aSteps++)
+            long[] result= MathUtils.solveLongDualEquation(xa, xb, x, ya, yb, y);
+            if (null!=result)
             {
-                long xRemaining=x-aSteps*xa;
-                if (xRemaining<0)
-                    break;
-                if (xRemaining%xb==0)
-                {
-                    final long bSteps = xRemaining / xb;
-                    if (aSteps*ya+bSteps*yb!=y)
-                    {
-                        continue;
-                    }
-                    long coins= aSteps * 3 + bSteps;
-                    if (coins<minCoins)
-                    {
-                        minCoins=coins;
-                    }
-                }
-            }
-            if (Long.MAX_VALUE==minCoins)
-                return 0;
-            return minCoins;
-        }
-
-        @Override
-        public String toString()
-        {
-            return "Machine{" +
-                   "xa=" + xa +
-                   ", xb=" + xb +
-                   ", ya=" + ya +
-                   ", yb=" + yb +
-                   ", x=" + x +
-                   ", y=" + y +
-                   '}';
-        }
-
-        // we actually need to solve a linear equation system
-        // lets cheat a bit and use Commons Math
-        public long solveLarge()
-        {
-            RealMatrix coefficients =
-                    new Array2DRowRealMatrix(new double[][] { { xa, xb }, { ya, yb } },
-                            false);
-            DecompositionSolver solver = new LUDecomposition(coefficients).getSolver();
-            RealVector constants = new ArrayRealVector(new double[] { x, y }, false);
-            RealVector solution = solver.solve(constants);
-            double aStepsD = solution.getEntry(0);
-            double bStepsD = solution.getEntry(1);
-            long aSteps=Math.round(aStepsD);
-            long bSteps=Math.round(bStepsD);
-            // verify that we have an integer solution
-            if (aSteps*xa+bSteps*xb==x && aSteps*ya+bSteps*yb==y)
-            {
-                return aSteps*3+bSteps;
+                return result[0]*3+result[1];
             }
             return 0;
         }
--- java/src/util/de/hendriklipka/aoc/MathUtils.java
+++ java/src/util/de/hendriklipka/aoc/MathUtils.java
@@ -2,6 +2,8 @@ package de.hendriklipka.aoc;
 
 import org.apache.commons.math3.util.ArithmeticUtils;
 
+import java.math.BigInteger;
+
 /**
  * User: hli
  * Date: 08.12.23
@@ -20,5 +22,35 @@ public class MathUtils
             pos++;
         }
         return lcm;
+    }
+
+    /**
+     * Solve a system of linear equations, where everything must be integer values
+     * The equations are xa*a+xb*b=x ; ya*a+yb*b=y
+     * The result is an array [a,b], or null when no integer solution can be found
+     */
+    public static long[] solveLongDualEquation(long xa, long xb, long x, long ya, long yb, long y)
+    {
+        BigInteger a1=BigInteger.valueOf(x).multiply(BigInteger.valueOf(yb)).subtract(BigInteger.valueOf(xb).multiply(BigInteger.valueOf(y)));
+        BigInteger b1=BigInteger.valueOf(y).multiply(BigInteger.valueOf(xa)).subtract(BigInteger.valueOf(ya).multiply(BigInteger.valueOf(x)));
+        BigInteger div=BigInteger.valueOf(xa).multiply(BigInteger.valueOf(yb)).subtract(BigInteger.valueOf(xb).multiply(BigInteger.valueOf(ya)));
+        if (div.signum()==0)
+            return null;
+        if (div.signum()==-1)
+        {
+            a1=a1.negate();
+            b1=b1.negate();
+            div=div.negate();
+        }
+        if (a1.mod(div).equals(BigInteger.ZERO) && b1.mod(div).equals(BigInteger.ZERO))
+        {
+            long a=a1.divide(div).longValue();
+            long b=b1.divide(div).longValue();
+            return new long[]{a,b};
+        }
+        else
+        {
+            return null;
+        }
     }
 }
