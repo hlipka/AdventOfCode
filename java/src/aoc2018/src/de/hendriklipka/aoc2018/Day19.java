e755bf327c6202a7a21b90691f2649fb050c717e
hli <hli-papermoon@hendriklipka.de>
2025-02-08 23:25:35 +0100

    finalize 2018 day 19

--- java/src/aoc2018/src/de/hendriklipka/aoc2018/Day19.java
+++ java/src/aoc2018/src/de/hendriklipka/aoc2018/Day19.java
@@ -19,16 +19,25 @@ public class Day19 extends AocPuzzle
     protected Object solvePartA() throws IOException
     {
         Day16.Machine m=new Day16.Machine(new ArrayList<>(List.of(0, 0, 0, 0, 0, 0)));
-        runComputer(m);
-        return m.getRegs().get(0);
-    }
-
-    private void runComputer(final Day16.Machine m) throws IOException
-    {
         final List<String> programLines = data.getLines();
         String pcRegDef=programLines.get(0);
         int pcReg=AocParseUtils.parseIntFromString(pcRegDef, "#ip (\\d+)");
         programLines.remove(0);
+        final var program = parseProgram(programLines);
+        int pc=0;
+        do
+        {
+            m.getRegs().set(pcReg, pc);
+            final Pair<String, List<Integer>> step = program.get(pc);
+            m.runCommand(step.getLeft(), step.getRight().get(0), step.getRight().get(1), step.getRight().get(2));
+            pc = m.getRegs().get(pcReg);
+            pc++;
+        } while (pc < program.size() && pc >= 0);
+        return m.getRegs().get(0);
+    }
+
+    private static List<Pair<String, List<Integer>>> parseProgram(final List<String> programLines)
+    {
         List<Pair<String, List<Integer>>> program = new ArrayList<>();
         for (String line: programLines)
         {
@@ -38,25 +47,36 @@ public class Day19 extends AocPuzzle
             List<Integer> command = new ArrayList<>(parts.stream().map(Integer::parseInt).toList());
             program.add(Pair.of(cmd, command));
         }
-        int pc=0;
-        while (true)
-        {
-            m.getRegs().set(pcReg, pc);
-            final Pair<String, List<Integer>> step = program.get(pc);
-            m.runCommand(step.getLeft(), step.getRight().get(0), step.getRight().get(1), step.getRight().get(2));
-            pc= m.getRegs().get(pcReg);
-            pc++;
-            if (pc>=program.size() || pc<0)
-                break;
-
-        }
+        return program;
     }
 
     @Override
     protected Object solvePartB() throws IOException
     {
         Day16.Machine m=new Day16.Machine(new ArrayList<>(List.of(1, 0, 0, 0, 0, 0)));
-        runComputer(m);
-        return m.getRegs().get(0);
+        final List<String> programLines = data.getLines();
+        String pcRegDef=programLines.get(0);
+        int pcReg=AocParseUtils.parseIntFromString(pcRegDef, "#ip (\\d+)");
+        programLines.remove(0);
+        final var program = parseProgram(programLines);
+        int pc=0;
+        do
+        {
+            m.getRegs().set(pcReg, pc);
+            final Pair<String, List<Integer>> step = program.get(pc);
+            m.runCommand(step.getLeft(), step.getRight().get(0), step.getRight().get(1), step.getRight().get(2));
+            pc = m.getRegs().get(pcReg);
+            pc++;
+            // we need to run until we are at PC=1, since then the target number was calculated
+        } while (pc != 1);
+        int target=m.getRegs().get(4);
+        int result=0;
+        // find all factors of the target and sum them
+        for (int i=1;i<=    target;i++)
+        {
+            if (0==(target%i))
+                result+=i;
+        }
+        return result;
     }
 }
